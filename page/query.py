c28a1e59ed0f320b71639b8bd2b41455b49bd96f
Rangga1708 <ranggajaya.c@gmail.com>
2025-07-24 21:16:28 +0700

    fix can't generate query with one column & etc

--- page/query.py
+++ page/query.py
@@ -28,9 +28,16 @@ def app():
     
     if feature is None:
         return {}
-    elif feature is not None and feature not in features["name"].values:
+    
+    if feature not in features["name"].values:
         st.error(lockey("query_error_feature_not_exist"))
         return {}
+        
+    if "feature" not in st.session_state:
+        st.session_state.feature = feature
+    elif st.session_state != feature:
+        del st.session_state.data_configs
+        st.session_state.feature = feature
 
     feature_id = find_value_in_dataframe.execute(
         data = features,
@@ -56,41 +63,47 @@ def execute_feature(features, tables, feature_id, feature_name):
     )
 
     st.header(lockey("header_notes"))
-    st.markdown(
-        f"""
-        {notes}
-        """
-    )
+    st.markdown(f"""{notes}""")
 
     st.divider()
+
+    if "data_configs" not in st.session_state:
+        st.session_state.data_configs = dict()
+        for table in tables_to_be_executed.itertuples(index=True):
+            columns = pd.DataFrame(table.columns)
+            columns["column_config"] = columns.apply(lambda column: define_column_config(column), axis = 1)
+            columns_name = columns["name"].tolist()
+            column_config = columns["column_config"].tolist()
+            
+            data = pd.DataFrame(
+                columns = columns_name,
+                dtype = "object"
+            )
+            column_config = dict(zip(columns_name, column_config))
+
+            st.session_state.data_configs[f"data-{table.id}"] = {
+                "data": data,
+                "column_config": column_config
+            }
 
     for table in tables_to_be_executed.itertuples(index=True):
         with st.form(key = "form-" + table.id, clear_on_submit = False):
             st.write(lockey("query_label_table_name") + table.table_name)
-            
-            columns = pd.DataFrame(table.columns)
-            columns["column_config"] = columns.apply(lambda column: define_column_config(column), axis = 1)
-            columns_name = columns["name"].tolist()
-            columns_type = [pd.Series(dtype="string") for i in range(len(columns_name))]
-            column_config = columns["column_config"].tolist()
 
-            data = pd.DataFrame(dict(zip(columns_name, columns_type)))
-            column_config = dict(zip(columns_name, column_config))
-
-            data = st.data_editor(
-                data = data,
+            st.session_state.data_configs[f"data-{table.id}"]["data"] = st.data_editor(
+                data = st.session_state.data_configs[f"data-{table.id}"]["data"],
                 num_rows = "dynamic",
-                column_config = column_config,
-                key = "data-" + table.id
+                column_config = st.session_state.data_configs[f"data-{table.id}"]["column_config"],
+                key = f"data-{table.id}"
             )
 
             downloaded = st.form_submit_button(lockey("query_button_confirmation"))
             
         if downloaded:
-            download_query(data,
+            download_query(st.session_state.data_configs[f"data-{table.id}"]["data"],
                             feature_name,
                             table.table_name,
-                            columns_name,
+                            st.session_state.data_configs[f"data-{table.id}"]["data"].columns,
                             table.query_select,
                             table.query_execute)
 
@@ -114,19 +127,24 @@ def download_query(data, feature_name, table_name, columns_name, query_select, q
     for column in columns_name:
         query_select = query_select.replace(f"{{{column}}}", data_select[column])
     
-    data["query"] = data.apply(lambda row: generate_execute_query(row, query_execute, columns_name), axis = 1)
-    query_execute = " \n\n".join(data["query"])
+    sql_keywords = ["select", "update", "merge", "delete", "insert"]
+    if any(keyword.lower() in query_execute.lower() for keyword in sql_keywords):
+        data["query"] = data.apply(lambda row: generate_execute_query(row, query_execute, columns_name), axis = 1)
+        query_execute = " \n\n".join(data["query"])
     
     all_query = f"--SELECT--\n{query_select}\n\n--EXECUTE--\n{query_execute}"
     filename = feature_name + " - " + table_name + ".sql"
 
     st.write(lockey("query_description_download_confirmation"))
 
-    if st.download_button(
+    download_button = st.download_button(
         label = lockey("query_button_download"),
         data = all_query,
         file_name = filename,
-        mime="text/plain"):
+        mime="text/plain"
+    )
+
+    if download_button:
         st.rerun()
 
 def generate_execute_query(row, query, columns_name):
